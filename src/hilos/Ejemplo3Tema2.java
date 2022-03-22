9f403b1cdc5e4f02f4fe73cccbdc20ede47f4dd9
Pedro Martin Rodeles <pedromartinrodeles@gmail.com>
2022-03-22 14:01:29 +0100

    Versión inicial

---
+++ src/hilos/Ejemplo3Tema2.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hilos;
+
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author AlumnoM
+ */
+public class Ejemplo3Tema2 {
+
+    static int NUMERO_HILOS = 2;
+    static int NUMERO_ITERACIONES = 4;
+
+    public static void main(String[] args) {
+
+        ArrayList<Hilo> hilos = new ArrayList<>();
+
+        for (int i = 0; i < NUMERO_HILOS; i++) {
+            hilos.add(new Hilo(NUMERO_ITERACIONES, "hilo " + i));
+        }
+
+        for (Hilo x : hilos) {
+            x.start();
+        }
+
+        try {
+            for (Hilo y : hilos) {
+                y.join();
+            }
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+
+        System.out.println("PROGRAMA FINALIZADO");
+    }
+}
---
+++ src/hilos/Hilo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hilos;
+
+/**
+ *
+ * @author AlumnoM
+ */
+public class Hilo extends Thread {
+
+    private int numeroDeRepeticiones;
+    //private int id;
+
+    Hilo(int numeroDeRepeticiones, String nombre) {
+        this.numeroDeRepeticiones = numeroDeRepeticiones;
+        System.out.println("Creando");
+        setName(nombre);
+    }
+
+    public void run() {
+        for (int i = 0; i < numeroDeRepeticiones; i++) {
+            System.out.println("Mensaje del hilo " + getName() + " en la intaracion " + i);
+        }
+    }
+}
